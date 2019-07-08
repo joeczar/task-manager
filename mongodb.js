72c92743532b080e5dd2c12d0e1fe85ba992fb1d
Joe Czarnecki <joe.czar@outlook.com>
2019-07-08 12:47:41 +0200

    77. The ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,15 @@
 // CRUD create read update delete
 /*jshint esversion: 6 */
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectId } = require('mongodb');
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const dbName = 'task-manager';
+
+const id = new ObjectId();
+console.log(id);
+console.log(id.getTimestamp());
+
+
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
   if (error) {
@@ -13,16 +18,17 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
   
   const db = client.db(dbName);
   
-  // db.collection('users').insertOne({
-  //   name: 'Joe',
-  //   age: 38
-  // }, (error, result) => {
-  //   if (error) {
-  //     return console.log('Unable to insert user.');
-  //   }
-  //   console.log(result.ops);
-    
-  // });
+  db.collection('users').insertOne({
+    _id: id,
+    name: 'Poncho',
+    age: 99
+  }, (error, result) => {
+    if (error) {
+      return console.log('Unable to insert user.');
+    }
+    console.log(result.ops);   
+  });
+  
   // db.collection('users').insertMany([
   //   {
   //   name: 'Ronja',
@@ -39,22 +45,22 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
     
   // });
 
-  db.collection('tasks').insertMany([
-    {
-      task: "Call insurance",
-      completed: false
-    }, {
-      task: "Blood test",
-      completed: false
-    }, {
-      task: "Finish challenge",
-      completed: true
-    }
-  ], (error, result) => {
-    if (error) {
-      return console.log('Unable to insert tasks');
-    }
-    console.log(result.ops);
+  // db.collection('tasks').insertMany([
+  //   {
+  //     task: "Call insurance",
+  //     completed: false
+  //   }, {
+  //     task: "Blood test",
+  //     completed: false
+  //   }, {
+  //     task: "Finish challenge",
+  //     completed: true
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert tasks');
+  //   }
+  //   console.log(result.ops);
     
-  });
+  // });
 });
