781f76a106090906e93a137dfa2cff3b8f400a44
Joe Czarnecki <joe.czar@outlook.com>
2019-07-09 16:30:24 +0200

    80. Updating Documents

--- mongodb.js
+++ mongodb.js
@@ -1,41 +1,71 @@
 // CRUD create read update delete
 /*jshint esversion: 6 */
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ObjectId } = require("mongodb");
 
-const connectionUrl = 'mongodb://127.0.0.1:27017';
-const dbName = 'task-manager';
+const connectionUrl = "mongodb://127.0.0.1:27017";
+const dbName = "task-manager";
 
-MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
-  if (error) {
-    return console.log('unable to connect');
-  }
-  
-  const db = client.db(dbName);
-  
-  // db.collection('users').findOne({ _id: ObjectId('5d231dea9d067310f45e53b9') }, (error, user) => {
-  //   if (error) {
-  //     console.log('Unable to fetch user.');
-  //   }
-  //   console.log(user);
-    
-  // });
-
-  // db.collection('users').find({ age: 38 }).toArray((error, results) => {
-  //   console.log(results);
-  // });
-  // db.collection('users').find({ age: 38 }).count((error, count) => {
-  //   console.log(count);
-  // });
-
-  db.collection('tasks').findOne({ _id: ObjectId('5d2213b5f164ce262470f922') }, (error, result) => {
-    
-    if(error) {
-      console.log("That didn't work");
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("unable to connect");
     }
-    console.log(result);
-  });
 
-  db.collection('tasks').find({ completed: false }).toArray((error, results) => {
-    console.log(results);
-  });
-});
+    const db = client.db(dbName);
+
+    // db.collection("users")
+    //   .updateOne(
+    //     {
+    //       _id: ObjectId("5d220fedd43d0c4a5080940d")
+    //     },
+    //     {
+    //       $set: {
+    //         name: "Dickhead"
+    //       }
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+
+    // db.collection("users")
+    //   .updateOne(
+    //     {
+    //       _id: ObjectId("5d220fedd43d0c4a5080940d")
+    //     },
+    //     {
+    //       $inc: {
+    //         age: 1
+    //       }
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+    db.collection("tasks")
+      .updateMany(
+        {
+          completed: true
+        },
+        {
+          $set: {
+            completed: false
+          }
+        }
+      )
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+);
