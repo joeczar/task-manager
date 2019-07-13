91a2fa2a6e853b8c71dd2b1b220efed5e444b49f
Joe Czarnecki <joe.czar@outlook.com>
2019-07-13 17:21:28 +0200

    91. Rescource creation endpoints. Part II

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /*jshint esversion: 6 */
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/User');
+const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,9 +11,19 @@ app.use(express.json());
 
 app.post('/users', (req, res) => {
   const user = new User(req.body);
-
   user.save().then(() => {
-    res.send(user);
+    res.status(201).send(user);
+  }).catch((e) => {
+    res.status(400);
+    res.send(e);
+  });
+});
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+
+  task.save().then(() => {
+    res.status(201).send(task);
   }).catch((e) => {
     res.status(400);
     res.send(e);
