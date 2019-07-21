081c19431dda819d018f9212b8aab3bfb3a29dc2
Joe Czarnecki <joe.czar@outlook.com>
2019-07-21 20:44:44 +0200

    108. Express Middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ const taskRouter = require("./routers/tasks");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//  if (req.method === 'GET') {
+//    res.send('GET requests are disabled');
+//  } else {
+//    next();
+//  }
+// });
+
+app.use((req, res, next) => {
+  res.status(503).send('Site down due to maintenence');
+ });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
